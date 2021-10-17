86a21e2f165f4c276ebefaa3519887eb5dd4143e
UmbertoJr <u.mele.coding@gmail.com>
2021-10-17 23:25:08 +0200

    update 17.10

--- src/io_tsp.py
+++ src/io_tsp.py
@@ -37,7 +37,7 @@ class ProblemInstance:
             self.points[i, 2] = line_i[2]
 
         self.create_dist_matrix()
-        if self.file_name in ["./problems/eil76.tsp", "./problems/kroA100.tsp", "../problems/eil76.tsp",
+        if self.file_name in ["AI2021BS/problems/eil76.tsp", "AI2021BS/problems/kroA100.tsp", "../problems/eil76.tsp",
                               "../problems/kroA100.tsp"]:
             self.exist_opt = True
             file_object = open(self.file_name.replace(".tsp", ".opt.tour"))
